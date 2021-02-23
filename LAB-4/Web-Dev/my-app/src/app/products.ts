20c2a6d3955f9f4917901f054963a769d32b5127
Khamitov Darkhan <khamitov.darkhan@Air-Darhan.kbtu.kz>
2021-02-24 01:15:03 +0600

    lab4

---
+++ LAB-4/Web-Dev/my-app/src/app/products.ts
@@ -0,0 +1,141 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Vertical Stand with Cooling Fan for PS5 Console and PS5 Digital Edition',
+    price: 15.99,
+    rating : "5/5",
+    description: 'Designed for PS5: Vertical stand, Cooling Storage Playstation 5 console with dual wireless controller charger station, Keep your PS5 console and DualSense wireless controllers organized, 15 game slots',
+    image1: "https://images-na.ssl-images-amazon.com/images/I/61mrs6QaG4L._SX522_.jpg",
+    image2: "https://images-na.ssl-images-amazon.com/images/I/71Y8W14pH1L._AC_SX679_.jpg",
+    image3: "https://images-na.ssl-images-amazon.com/images/I/71WE32MLpxL._AC_SX679_.jpg",  
+    href: 'https://www.amazon.com/dp/B08P14WBV7/ref=sspa_dk_detail_1?psc=1&pd_rd_i=B08P14WBV7&pd_rd_w=UKM9A&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=zVm0I&pf_rd_r=76G5AFW4XDQ9VQF5SYT8&pd_rd_r=0b538e0d-3326-4b5d-813a-17cd34a96824&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzNFVSMUpCMVpRQkdKJmVuY3J5cHRlZElkPUEwMDY3NTczTE0xR1BORE4yQ1ZFJmVuY3J5cHRlZEFkSWQ9QTA4MjcxMDFVOE5QWkhMUjFGOUQmd2lkZ2V0TmFtZT1zcF9kZXRhaWwmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl',
+    fb: 'https://www.facebook.com/dialog/share?_encoding=UTF8&app_id=465632727431967&display=popup&href=https%3A%2F%2Fwww.amazon.com%2Fdp%2FB079Y45KTJ%2Fref%3Dcm_sw_r_fa_dp_ZZGDAMXN6WWPGHRJE123&redirect_uri=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fpdp%2Ftaf%2Futility%2Ffb_post_redirect.html%2Fref%3Dcm_sw_s_fa_dp_ZZGDAMXN6WWPGHRJE123&hashtag=%23Amazon',
+  },
+  {
+    id: 2,
+    name: 'Maneater - PlayStation 5',
+    price: 32.29,
+    rating : "5/5",
+    description: 'Unique story - play through a full narrative, story-based campaign narrated by Chris parnell (saturday night live, 30 Rock, Rick and Morty) and set against the backdrop of a reality TV show',
+    image1 : 'https://images-na.ssl-images-amazon.com/images/I/81RwRfnlQKL._SX385_.jpg',
+    image2 : 'https://images-na.ssl-images-amazon.com/images/I/81zl1g%2BBeQL._SX522_.jpg',
+    image3 : 'https://images-na.ssl-images-amazon.com/images/I/81OkpdaPmML._SX522_.jpg',
+    href: 'https://www.amazon.com/Maneater-PlayStation-5/dp/B08KWM1C6Y/ref=pd_sbs_7?pd_rd_w=htYiR&pf_rd_p=c52600a3-624a-4791-b4c4-3b112e19fbbc&pf_rd_r=R03EH3F5PKEEFDXMWJR1&pd_rd_r=d14a36bc-8704-4c5f-b68d-6465ccafd84b&pd_rd_wg=COQHl&pd_rd_i=B08KWM1C6Y&psc=1',
+    fb: 'https://www.facebook.com/dialog/share?_encoding=UTF8&app_id=465632727431967&display=popup&href=https%3A%2F%2Fwww.amazon.com%2Fdp%2FB08KWM1C6Y%2Fref%3Dcm_sw_r_fa_dp_DDF4700X1PSXWKMMDH75%3F_encoding%3DUTF8%26psc%3D1&redirect_uri=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fpdp%2Ftaf%2Futility%2Ffb_post_redirect.html%2Fref%3Dcm_sw_s_fa_dp_DDF4700X1PSXWKMMDH75%3Fie%3DUTF8%26psc%3D1&hashtag=%23Amazon'
+  },
+  {
+    id: 3,
+    name: 'YOUMAKER Metallic Designed for iPhone XR Case',
+    price: 16.99,
+    rating : "5/5",
+
+    description: 'For iPhone XR Case: ONLY Compatible with Apple iPhone XR 6.1 inch. DOES NOT compatible with iPhone X/Xs/Xs Max',
+    image1 : 'https://images-na.ssl-images-amazon.com/images/I/71F3aBdNViL._AC_SX679_.jpg',
+    image2 : 'https://images-na.ssl-images-amazon.com/images/I/71P204dOCPL._AC_SX679_.jpg',
+    image3 : 'https://images-na.ssl-images-amazon.com/images/I/71Njm371pPL._AC_SX679_.jpg', 
+    href :'https://www.amazon.com/YOUMAKER-iPhone-Shockproof-Protector-Full-Body/dp/B0887X3N6F/ref=bmx_4?pd_rd_w=6Vl7i&pf_rd_p=f71775c5-9fe6-4e06-a8bc-a95de49acaab&pf_rd_r=EC57B2ZTK63N2QKQY2TT&pd_rd_r=219b25d5-cfe8-406f-b5f1-d0eda8c74db7&pd_rd_wg=9bbbp&pd_rd_i=B0887X3N6F&psc=1',
+    fb : 'https://www.amazon.com/gp/redirect.html/ref=cm_sw_cl_fa_dp_X9RFE48C0HCPDAWWBBNT?_encoding=UTF8&location=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Fshare%3F_encoding%3DUTF8%26app_id%3D465632727431967%26display%3Dpopup%26href%3Dhttps%253A%252F%252Fwww.amazon.com%252Fdp%252FB0887X3N6F%252Fref%253Dcm_sw_r_fa_dp_X9RFE48C0HCPDAWWBBNT%253F_encoding%253DUTF8%2526psc%253D1%26redirect_uri%3Dhttps%253A%252F%252Fwww.amazon.com%252Fgp%252Fpdp%252Ftaf%252Futility%252Ffb_post_redirect.html%252Fref%253Dcm_sw_s_fa_dp_X9RFE48C0HCPDAWWBBNT%253Fie%253DUTF8%2526psc%253D1%26hashtag%3D%2523Amazon&token=91A75F93C11EB1D0DCF1ECE724DE2117FFCB7064',
+  },
+
+
+  {
+    id: 4,
+    name: 'AMD Ryzen 5 3600 6-Core, 12-Thread Unlocked Desktop Processor with Wraith Stealth Cooler',
+    price: 199.99,
+    rating : "5/5",
+
+    description: 'The worlds most advanced processor in the desktop PC gaming segment',
+    image1 : 'https://images-na.ssl-images-amazon.com/images/I/71WPGXQLcLL._AC_SX679_.jpg',
+    image2 : 'https://images-na.ssl-images-amazon.com/images/I/716hAjT1uUL._AC_SX679_.jpg',
+    image3 : 'https://images-na.ssl-images-amazon.com/images/I/61-jkNrO3NL._AC_SX679_.jpg', 
+    href :'https://www.amazon.com/AMD-Ryzen-3600-12-Thread-Processor/dp/B07STGGQ18/ref=sr_1_2?dchild=1&qid=1614102732&s=computers-intl-ship&sr=1-2',
+    fb: 'https://www.facebook.com/dialog/share?_encoding=UTF8&app_id=465632727431967&display=popup&href=https%3A%2F%2Fwww.amazon.com%2Fdp%2FB0887X3N6F%2Fref%3Dcm_sw_r_fa_dp_R5QM9QR7DNZSDZHHBHRP%3F_encoding%3DUTF8%26psc%3D1&redirect_uri=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fpdp%2Ftaf%2Futility%2Ffb_post_redirect.html%2Fref%3Dcm_sw_s_fa_dp_R5QM9QR7DNZSDZHHBHRP%3Fie%3DUTF8%26psc%3D1&hashtag=%23Amazon'
+  },
+  {
+    id: 5,
+    name: 'HP VH240a 23.8-Inch Full HD 1080p IPS LED Monitor with Built-In Speakers and VESA Mounting',
+    price: 163.00,
+    rating : "5/5",
+
+    description: 'RESOLUTION & PANEL — 23.8-inch Full HD monitor (1920 x 1080p @ 60 Hz) with 16:9 aspect ratio and an anti-glare matte IPS LED-backlit panel (2 million pixels, 16.7 million colors)',
+    image1 : 'https://images-na.ssl-images-amazon.com/images/I/71trhuzbhML._AC_SY879_.jpg',
+    image2 : 'https://images-na.ssl-images-amazon.com/images/I/81Gq8HvMSUL._AC_SY879_.jpg',
+    image3 : 'https://images-na.ssl-images-amazon.com/images/I/81xKeeWHFEL._AC_SY879_.jpg', 
+    href :'https://www.amazon.com/HP-23-8-inch-Adjustment-Speakers-VH240a/dp/B072M34RQC/ref=sr_1_3?dchild=1&qid=1614102955&s=computers-intl-ship&sr=1-3',
+    fb : 'https://www.facebook.com/dialog/share?_encoding=UTF8&app_id=465632727431967&display=popup&href=https%3A%2F%2Fwww.amazon.com%2Fdp%2FB072M34RQC%2Fref%3Dcm_sw_r_fa_dp_EXRKTWZQ21R2S9TXPFYT&redirect_uri=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fpdp%2Ftaf%2Futility%2Ffb_post_redirect.html%2Fref%3Dcm_sw_s_fa_dp_EXRKTWZQ21R2S9TXPFYT&hashtag=%23Amazon'
+  },
+  {
+    id: 6,
+    name: 'Sony MDRZX110/BLK ZX Series Stereo Headphones (Black)',
+    price: 299,
+    rating : "5/5",
+
+    description: '30 millimeter drivers for rich, full frequency response.Lightweight and comfortable on ear design',
+    image1 : 'https://images-na.ssl-images-amazon.com/images/I/61Qb9N6wq-L._AC_SX679_.jpg',
+    image2 : 'https://images-na.ssl-images-amazon.com/images/I/71N772sN14L._AC_SX679_.jpg',
+    image3 : 'https://images-na.ssl-images-amazon.com/images/I/71jQZy3ZzxL._AC_SY879_.jpg', 
+    href :'https://www.amazon.com/Sony-MDRZX110-BLK-Stereo-Headphones/dp/B00NJ2M33I',
+    fb: 'https://www.amazon.com/gp/redirect.html/ref=cm_sw_cl_fa_dp_0FV7FVC7XBC5JHG7ECJD?_encoding=UTF8&location=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Fshare%3F_encoding%3DUTF8%26app_id%3D465632727431967%26display%3Dpopup%26href%3Dhttps%253A%252F%252Fwww.amazon.com%252Fdp%252FB00NJ2M33I%252Fref%253Dcm_sw_r_fa_dp_0FV7FVC7XBC5JHG7ECJD%26redirect_uri%3Dhttps%253A%252F%252Fwww.amazon.com%252Fgp%252Fpdp%252Ftaf%252Futility%252Ffb_post_redirect.html%252Fref%253Dcm_sw_s_fa_dp_0FV7FVC7XBC5JHG7ECJD%26hashtag%3D%2523Amazon&token=0E10D8918FFD8AAE19BFB871A4772930063B1CCC'
+  },
+  {
+    id: 7,
+    name: 'PeohZarr On-Ear Headphones with Microphone, Lightweight Folding Stereo Bass Headphones with 1.5M Tangle Free Cord',
+    price: 12.92,
+    rating : "5/5",
+
+    ddescription: 'These headphones with microphone adopt High-definition 40mm driver and compact earcups design, which deliver exceptionally clear sound with full dynamic range and rich bass and crisp mids. Effortless style and powerful sound let you tune into your own rhythms with the set of on-ear headphones',
+    image1 : 'https://images-na.ssl-images-amazon.com/images/I/71P3igzuWCL._AC_SY606_.jpg',
+    image2 : 'https://images-na.ssl-images-amazon.com/images/I/61aW3eYkebL._AC_SY606_.jpg',
+    image3 : 'https://images-na.ssl-images-amazon.com/images/I/71upE-U5nuL._AC_SY606_.jpg', 
+    href :'https://www.amazon.com/PeohZarr-Headphones-Microphone-Lightweight-Smartphone/dp/B07SHH37M6/ref=sr_1_10?dchild=1&qid=1614103166&s=electronics&sr=1-10',
+    fb: 'https://www.facebook.com/dialog/share?_encoding=UTF8&app_id=465632727431967&display=popup&href=https%3A%2F%2Fwww.amazon.com%2Fdp%2FB00NJ2M33I%2Fref%3Dcm_sw_r_fa_dp_ZTT0QYX3GRHFQWEHZYH8&redirect_uri=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fpdp%2Ftaf%2Futility%2Ffb_post_redirect.html%2Fref%3Dcm_sw_s_fa_dp_ZTT0QYX3GRHFQWEHZYH8&hashtag=%23Amazon'
+  },
+  {
+    id: 8,
+    name: 'Sony NEW Alpha 7S III Full-frame Interchangeable Lens Mirrorless Camera',
+    price: 3480,
+    rating : "5/5",
+
+    description: '12.1MP1 Exmor R sensor, optimized for 4K, sensitivity and speed New BIONZ XR processing engine delivers 8x more processing power2 Low noise images w/ ISO up to 409,6003 and 15+stop dynamic range4 Video capture resolution: 4K UHD 2160p',
+    image1 : 'https://images-na.ssl-images-amazon.com/images/I/81MpiBUkqsL._AC_SX450_.jpg',
+    image2 : 'https://images-na.ssl-images-amazon.com/images/I/816xB78wpLL._AC_SX450_.jpg',
+    image3 : 'https://images-na.ssl-images-amazon.com/images/I/71q-YoZLFjL._AC_SX450_.jpg', 
+    href :'https://www.amazon.com/Sony-Full-frame-Interchangeable-Mirrorless-Camera/dp/B08DP4NKGN/ref=sr_1_1?dchild=1&qid=1614103569&rnid=502394&s=electronics&sr=1-1',
+    fb : 'https://www.facebook.com/dialog/share?_encoding=UTF8&app_id=465632727431967&display=popup&href=https%3A%2F%2Fwww.amazon.com%2Fdp%2FB07SHH37M6%2Fref%3Dcm_sw_r_fa_dp_PG7DWKM1JAWY3ZYF155Z&redirect_uri=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fpdp%2Ftaf%2Futility%2Ffb_post_redirect.html%2Fref%3Dcm_sw_s_fa_dp_PG7DWKM1JAWY3ZYF155Z&hashtag=%23Amazon'
+  },
+  
+  {
+    id: 9,
+    name: 'Kodak FunSaver 35mm Single Use Camera',
+    price: 14.97,
+    rating : "5/5",
+
+    description: 'Product Type:Camera Film Item Package Dimension:3.81 Cm L X 6.604 Cm W X 13.462 Cm HItem Package Weight:0.159 Kg Country Of Origin: United States',
+    image1 : 'https://images-na.ssl-images-amazon.com/images/I/61uK61nr7yL._AC_SX450_.jpg',
+    image2 : '',
+    image3 : '', 
+    href :'https://www.amazon.com/Kodak-FunSaver-35mm-Single-Camera/dp/B00001R3W3/ref=sr_1_3?dchild=1&qid=1614103569&rnid=502394&s=electronics&sr=1-3',
+    fb: 'https://www.facebook.com/dialog/share?_encoding=UTF8&app_id=465632727431967&display=popup&href=https%3A%2F%2Fwww.amazon.com%2Fdp%2FB00001R3W3%2Fref%3Dcm_sw_r_fa_dp_RXW3J90WD8JKM57KH757&redirect_uri=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fpdp%2Ftaf%2Futility%2Ffb_post_redirect.html%2Fref%3Dcm_sw_s_fa_dp_RXW3J90WD8JKM57KH757&hashtag=%23Amazon'
+  },
+  
+  {
+    id: 10,
+    name: 'Canon EOS M50 Mirrorless Vlogging Camera Kit with EF-M 15-45mm Lens, Black',
+    price: 649.00,
+    rating : "5/5",
+
+    description: 'Dual Pixel CMOS AF for fast, accurate autofocus that helps you get the photo you want right as the moment happens',
+    image1 : 'https://images-na.ssl-images-amazon.com/images/I/61HRnUdvv6L._AC_SY450_.jpg',
+    image2 : 'https://images-na.ssl-images-amazon.com/images/I/619fzR1aB2L._AC_SY450_.jpg',
+    image3 : 'https://images-na.ssl-images-amazon.com/images/I/51JbM17P4JL._AC_SY450_.jpg', 
+    href :'https://www.amazon.com/Canon-Mirrorless-Camera-EF-M15-45mm-Video/dp/B079Y45KTJ/ref=sr_1_8?dchild=1&qid=1614103569&rnid=502394&s=electronics&sr=1-8',
+    fb : 'https://www.facebook.com/dialog/share?_encoding=UTF8&app_id=465632727431967&display=popup&href=https%3A%2F%2Fwww.amazon.com%2Fdp%2FB079Y45KTJ%2Fref%3Dcm_sw_r_fa_dp_3NY8QXJ0AWS0YY67C87S&redirect_uri=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fpdp%2Ftaf%2Futility%2Ffb_post_redirect.html%2Fref%3Dcm_sw_s_fa_dp_3NY8QXJ0AWS0YY67C87S&hashtag=%23Amazon'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
